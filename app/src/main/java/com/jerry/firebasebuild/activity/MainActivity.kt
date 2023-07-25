b7aaa3f418691b7892fa112b47a2d0ee639a54ad
jerry <jerry.cho.dev@hotmail.com>
2023-07-25 17:36:15 +0100

    build again

--- app/src/main/java/com/jerry/firebasebuild/activity/MainActivity.kt
+++ app/src/main/java/com/jerry/firebasebuild/activity/MainActivity.kt
@@ -5,10 +5,8 @@ import android.os.Bundle
 import com.jerry.firebasebuild.R
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
 }
